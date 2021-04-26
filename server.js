53740ef515ef2d02f06641b2feb397608a853a99
Yinglun Yin <adsl7377@163.com>
2021-04-26 14:08:37 +0800

    final bug fixed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 52134
 
@@ -20,10 +21,26 @@ app.use(bodyParser.urlencoded
 ({extended: false}))
 app.use(bodyParser.json())
 
+const sessionConfig = {
+    resave: false,
+    saveUninitialized: false,
+    secret: "Test",
+    cookie : {
+        sameSite: 'none',
+    }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1); // trust first proxy
+    sessionConfig.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionConfig));
+
 const mongoose = require('mongoose');
-const databaseName = 'traveljournals'
-const userName = "user123"
-const password = "user123"
+// const databaseName = 'traveljournals'
+// const userName = "user123"
+// const password = "user123"
 
 mongoose.connect(
     "mongodb+srv://user123:user123@cluster0.ajbuj.mongodb.net/traveljournals?retryWrites=true&w=majority",
